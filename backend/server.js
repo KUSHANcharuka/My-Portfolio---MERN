4cbc87dbed753a8190f4e3a8bc67c52b30c42680
KUSHANcharuka <150827136+KUSHANcharuka@users.noreply.github.com>
2026-05-02 09:19:49 +0530

    Update Server.js

--- backend/server.js
+++ backend/server.js
@@ -8,21 +8,18 @@ dotenv.config();
 const app = express();
 
 // Middleware
-const clientOrigin = process.env.CLIENT_URL || "http://localhost:3714";
-// Allow CORS for localhost and deployed frontend
+const clientOrigin = process.env.CLIENT_URL;
+
+// || "http://localhost:3714"
+
 app.use(
   cors({
     origin: (origin, callback) => {
-      // Allow requests from localhost and no origin (like mobile apps)
       if (
         !origin ||
-        origin.includes("localhost") ||
-        origin.includes("127.0.0.1") ||
         origin.includes("https://my-portfolio-mern-ebon.vercel.app")
       ) {
         callback(null, true);
-      } else {
-        callback(null, true); // Allow all for now, restrict as needed
       }
     },
   }),
@@ -38,6 +35,6 @@ mongoose
   .then(() => console.log(" MongoDB connected")) // || "mongodb://localhost:27017/portfolio"
   .catch((err) => console.error(" MongoDB error:", err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
